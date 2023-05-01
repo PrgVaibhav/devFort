7e4d3f0ab5986da365751429d914d6182b1412d5
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-01 09:21:05 +0530

    Final touchup

--- pages/next-sitemap.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-// Default code you can customize according to your requirements.
-module.exports = {
-  siteUrl: "https://devfort.tech",
-  generateRobotsTxt: true, // (optional)
-  // REST CODE READ DOCS  ...
-};
