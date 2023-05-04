559b67ea73b5d8165ee85d5ee59917ca8a09be06
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-04 11:05:20 +0530

    Website Optimized

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -25,7 +25,7 @@ export default async function generateSitemap(req, res) {
   try {
     // Create the sitemap
     const sitemap = createSitemap({
-      hostname: "https://www.devfort.tech",
+      hostname: "https://www.devfort.tech/",
       urls,
     });
 
