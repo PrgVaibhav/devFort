2af5c8e8afeb041220d6f9214a159cc71be3126d
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-04 11:03:22 +0530

    Website Optimized

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,5 @@
 import { createSitemap } from "sitemap";
+
 const urls = [
   {
     url: "/",
@@ -21,16 +22,22 @@ const urls = [
 ];
 
 export default async function generateSitemap(req, res) {
-  // Create the sitemap
-  const sitemap = createSitemap({
-    hostname: "https://www.devfort.tech",
-    urls,
-  });
+  try {
+    // Create the sitemap
+    const sitemap = createSitemap({
+      hostname: "https://www.devfort.tech",
+      urls,
+    });
 
-  // Set the response headers
-  res.setHeader("Content-Type", "application/xml");
-  res.setHeader("Cache-Control", "public, max-age=86400, must-revalidate");
+    // Set the response headers
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Cache-Control", "public, max-age=86400, must-revalidate");
 
-  // Send the sitemap as the response
-  res.end(sitemap.toString());
+    // Send the sitemap as the response
+    res.status(200).send(sitemap.toString());
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).send("Error generating sitemap");
+  }
 }
