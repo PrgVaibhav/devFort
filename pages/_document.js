2567d86db9e0ab91115186b21d1bf22074e04797
prgVaibhav <vaibhav2k1507@gmail.com>
2023-05-04 09:56:36 +0530

    Website optimized

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,13 @@ export default function Document() {
           property="og:description"
           content="Dev Fort is a community where you will get access to all the resources for building projects, like Project Ideas, API resources, Images, Icons, and many more things."
         />
+        <meta name="author" content="Dev Fort" />
+        <meta name="keywords" content="Dev Fort, DevFort, DevFort.tech" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.devfort.tech" />
         <meta property="og:image" content="/devfort.webp" />
